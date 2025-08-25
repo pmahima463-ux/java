24609f2a1dd664c17dbb1768276b299fce00fcb0
Mahima prajapati <pmahima463@gmail.com>
2025-08-25 09:15:14 +0530

    Update addition.java
    addition using java

--- addition.java
+++ addition.java
@@ -1,11 +1,12 @@
 class Addition {
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 20;
+        int num1 = 15;
+        int num2 = 35;
         int sum = num1 + num2;
 
         System.out.println("First Number: " + num1);
         System.out.println("Second Number: " + num2);
         System.out.println("Sum = " + sum);
     }
+
 }
